a5c1b58e47c0b8afb80c364cecbbccecfa93b3d5
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-11-28 12:01:19 +0100

    delete useless ProgramController.php created for testing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
     public function index(): Response
